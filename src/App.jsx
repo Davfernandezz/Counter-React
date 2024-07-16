56da4d9ccaa7b2d658e0779f52cd51012948a8ac
David Fernadez <daffevafernandez@gmail.com>
2024-07-16 12:34:20 +0200

    feat: separate counter components

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import './App.css'
+import { Title } from './components/Title/Title';
+import { CButton } from './components/CButton/CButton';
+
+function App() {
+  //Declaracion de variables, funciones y codigo js
+  const title = "Counter"
+
+  // let initValue = 0;
+  const [initValue, setInitValue] = useState(0)
+
+  function decrement() {
+    console.log('decrement');
+    if (initValue === 0) {
+      return alert('No puede diminuir menos de cero')
+    }
+    setInitValue(initValue - 1)
+    console.log(initValue)
+  }
+
+  function increment() {
+    console.log('increment');
+    setInitValue(initValue + 1)
+    console.log(initValue)
+  }
+
+  function reset(){
+    setInitValue(0)
+  }
+
+  return (
+    <>
+      <Title nombre="Counter" />
+      <CButton content="decrement" click={decrement}/>
+      <div>{initValue}</div>
+      <CButton content="increment" click={increment}/>
+
+      <CButton content="reset" click={reset}/>
+    </>
+  )
+}
+export default App
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export const Title = ({ nombre }) => {
+  return (
+    <>
+      <h1>{nombre}</h1>
+    </>
+  );
+};
